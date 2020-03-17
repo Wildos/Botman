fba25e8cd5f0721f3890b178a3254830b22b8e47
catslord <tfernand@student.42.fr>
2020-03-17 22:41:17 +0100

    feat(react): Emoji reaction to critical roll
    React nat_1 when rolled critical 1
    React <class> when rolled critical 20

--- bot.js
+++ bot.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const BtmErr = require('./btm_error_handler');
 const BtmCmds = require('./bot_commands.js');
+
+const regex_rolled_nat_20 = new RegExp('<@(.*)>, .*\\(.*20.*\\) \\[\\/20\\].* = [0-9]+');
+const regex_rolled_nat_1 = new RegExp('<@(.*)>, .*\\((?:[0-9]+ \\+ )*1(?: \\+ [0-9]+)*\\) \\[\\/20\\].* = [0-9]+');
+
+const dnd_class = ['barbarian', 'bard', 'cleric', 'druid', 'dungeon_master', 'fighter', 'monk', 'paladin', 'ranger', 'rogue', 'sorcerer', 'warlock', 'wizard'];
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -11,6 +15,51 @@ function is_a_command(str) {
 	if (str.substring(0, 1) === '!') 
 		return true;
 	return false;
+}
+
+function react_to_message(msg, emoji_name){
+	let guild = msg.guild;
+	if (guild == undefined){
+		console.log('guild undefined');
+		return;
+	}
+	if (!guild.available){
+		console.log('guild not available: ' + guild.name);
+		return;
+	}
+	let emojis = guild.emojis;
+	let class_emoji_id = emojis.findKey(emoji => emoji.name === emoji_name);
+	if (class_emoji_id != null){
+		msg.react(class_emoji_id).catch(console.error);
+		console.log('reacted ' + emoji_name);
+	}
+	else
+		console.log('emoji not found: ' + emoji_name);
+}
+
+function react_class_from_message(msg){
+	let guild = msg.guild;
+	if (guild == undefined){
+		console.log('guild undefined');
+		return;
+	}
+	if (!guild.available){
+		console.log('guild not available: ' + guild.name);
+		return;
+	}
+	let author_id = msg.content.match(regex_rolled_nat_20)[1];
+	console.log('author_id found: ' + author_id);
+	dnd_class.forEach(class_name => {
+		let role = guild.roles.find(element => element.name == class_name);
+		if (role != undefined){
+			let user_role = role.members.find(element => element.id == author_id);
+			if (user_role != undefined){
+				console.log('user is a ' + class_name);
+				react_to_message(msg, class_name);
+			}
+		}
+	}
+	);
 }
 
 function parse_command_parts(str) {
@@ -28,7 +77,8 @@ function parse_command_parts(str) {
 client.on('message', msg => {
 	if (process.env.AUTHORIZED_CHANNELS.includes(msg.channel.name)
 		|| msg.channel.type === 'dm') {
-
+		console.log('message received by ' + msg.author.username + ': '
+			+ msg.content);
 		if (is_a_command(msg.content)) {
 			let commands_parts = parse_command_parts(msg.content);
 			const command = commands_parts[0];
@@ -36,8 +86,16 @@ client.on('message', msg => {
 
 			console.log('Command received by ' + msg.author.username + ': '
 			+ msg.content);
-			
+
 			BtmCmds.call_command(msg, command, args);
+		}
+		else {
+			if (msg.content.match(regex_rolled_nat_20)){
+				react_class_from_message(msg);
+			}
+			if (msg.content.match(regex_rolled_nat_1)){
+				react_to_message(msg, 'nat_1');
+			}
 		}
 	}
 });
